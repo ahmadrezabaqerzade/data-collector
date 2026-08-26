362490316237a42392411e10b906ceac486314db
ahmadreza <ahmadrezabaqerzadeh@gmail.com>
2026-08-26 04:40:30 +0330

    feat(main): fetch historical tables for all dates

--- main.py
+++ main.py
@@ -3,17 +3,31 @@ import pandas as pd
 from io import StringIO
 
 session = requests.Session()
-url = "https://www.tgju.org/profile/geram18/history"
+url = "https://api.tgju.org/v1/market/indicator/summary-table-data/geram18" 
+
+params = {
+    "lang": "fa",
+    "order_dir": "asc",
+    "draw": 3,
+    "start": 0,
+    "length": 1000000000000000000000000000000000,
+    "search": "",
+    "order_col": "",
+    "from": "1405/01/01",
+    "to": "",
+    "convert_to_ad": 1,
+}
 
 
 try:
-    response = session.get(url=url, timeout=5)
+    response = session.get(url=url, 
+                           params=params,
+                           timeout=5)
     response.raise_for_status()
     print(response.status_code)
 except requests.RequestException as e:
     print(e)
 else:
-    print("Contains table:", "<table" in response.text)
-    tables = pd.read_html(StringIO(response.text))
-    print(len(tables))
-    tables[0].to_csv('info.csv')
+    data = response.json()
+    df = pd.DataFrame(data['data'])
+    print(df)
