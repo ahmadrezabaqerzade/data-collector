5af34bbaa99270f09fc5cf97fe076ce034471280
ahmadreza <ahmadrezabaqerzadeh@gmail.com>
2026-08-27 16:46:02 +0330

    feat(collector): clean numeric values from HTML

--- collector.py
+++ collector.py
@@ -1,8 +1,25 @@
 import requests
 import pandas as pd
 import json
+import re 
 import time 
 
+
+def clean_value(x):
+    """
+    remove the HTML while preserving the underlying numeric type (int/float)
+    """
+    if isinstance(x, str):
+        matches = re.search(r".*>([\d\.]+).*<.*", x)
+        if matches:
+            x = matches.group(1)
+        elif "," in x:
+            x = x.replace(",", "")
+        try:
+            x = float(x) if "." in x else int(x)
+        except:
+            None 
+    return x
 
 class BaseDataCollector:
     def __init__(self, timeout=5, max_try=1, try_delay=1):
@@ -39,28 +56,41 @@ class TGJUDataCollector(BaseDataCollector):
         response = super().collect(url, params)
         try:
             data = response.json()
-            df = pd.DataFrame(data[self.param])
+            df = pd.DataFrame(data[self.param]).map(clean_value)
             return df
         except json.JSONDecodeError as e:
             print(e)
 
 
-url = "https://api.tgju.org/v1/market/indicator/summary-table-data/geram18" 
-timeout = 5
-max_try = 5
-params = {
-    "lang": "fa",
-    "order_dir": "asc",
-    "draw": 3,
-    "start": 0,
-    "length": 10000,
-    "search": "",
-    "order_col": "",
-    "from": "1405/01/01",
-    "to": "",
-    "convert_to_ad": 1,
-}
 
-data_collector = TGJUDataCollector('data', timeout=timeout, max_try=max_try)
-data = data_collector.collect(url=url, params=params)
-print(data)
+if __name__ == "__main__":
+
+    url = "https://api.tgju.org/v1/market/indicator/summary-table-data/geram18" 
+    timeout = 5
+    max_try = 5
+    params = {
+        "lang": "fa",
+        "order_dir": "asc",
+        "draw": 3,
+        "start": 0,
+        "length": 10000,
+        "search": "",
+        "order_col": "",
+        "from": "1405/01/01",
+        "to": "",
+        "convert_to_ad": 1,
+    }
+
+    columns = ["Open",
+               "Low",
+               "High",
+               "Close",
+               "Change",
+               "Change Percent",
+               "Date / Gregorian",
+               "Date / Solar Hijri"]
+
+    data_collector = TGJUDataCollector('data', timeout=timeout, max_try=max_try)
+    data = data_collector.collect(url=url, params=params)
+    data.columns = columns
+    print(data)
